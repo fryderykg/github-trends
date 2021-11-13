08140e9db36e2e437567a54933fb4ae1bc4b8fdc
eszkimos <eszkimos@gmail.com>
2021-11-13 12:01:13 +0100

    add styles to checkbox

--- src/views/checkbox/checkbox.tsx
+++ src/views/checkbox/checkbox.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './checkbox.module.scss';
 
 export type CheckboxProps = {
@@ -11,25 +12,27 @@ export type CheckboxProps = {
 
 const Checkbox = (props: CheckboxProps) => {
   return (
-    <div className={styles.checkboxContainer}
-         data-testid={'component_checkbox_container'}>
-
-      <label htmlFor={props.name}>
-        <input checked={props.checked}
-               data-testid={'component_checkbox_input'}
-               disabled={props.disabled}
-               id={props.name}
-               name={props.name}
-               onChange={props.onChange}
-               type="checkbox"
-               value={props.name}
-        />
-        <label htmlFor={props.name}
-               data-testid={'component_checkbox_label'}>
-          {props.label}
-        </label>
+    <label className={classNames(styles.checkboxContainer, {
+      [styles.isActive]: props.checked,
+    })}
+           data-testid={'component_checkbox_container'}
+           htmlFor={props.name}>
+      <input checked={props.checked}
+             className={styles.checkbox}
+             data-testid={'component_checkbox_input'}
+             disabled={props.disabled}
+             id={props.name}
+             name={props.name}
+             onChange={props.onChange}
+             type="checkbox"
+             value={props.name}
+      />
+      <label className={styles.label}
+             htmlFor={props.name}
+             data-testid={'component_checkbox_label'}>
+        {props.label}
       </label>
-    </div>
+    </label>
   );
 };
 
